e2c49e97c8389cdc33636af1116eb74927afcfe7
nurullzzz <75351301+nurullzzz@users.noreply.github.com>
2023-05-26 21:28:58 +0800

    10 folder belum berhasil tadi

--- train_val_test_split.py
+++ train_val_test_split.py
@@ -8,64 +8,57 @@ import random
 import os
 
 # Define paths to image folders
-image_path = '/content/images/all'
-train_path = '/content/images/train'
-val_path = '/content/images/validation'
-test_path = '/content/images/test'
+image_folder = '/content/images/all'
+train_folder = '/content/images/train'
+val_folder = '/content/images/validation'
+test_folder = '/content/images/test'
 
-# Get list of all images
-file_list = []
-for folder_name in ['baik', 'bangku', 'bel', 'dia', 'meja', 'pramuka', 'sakit', 'saya', 'teman', 'tugas']:
-    folder_path = os.path.join(image_path, folder_name)
-    jpg_files = list(Path(folder_path).rglob('*.jpg'))
-    JPG_files = list(Path(folder_path).rglob('*.JPG'))
-    png_files = list(Path(folder_path).rglob('*.png'))
-    bmp_files = list(Path(folder_path).rglob('*.bmp'))
-    file_list.extend(jpg_files + JPG_files + png_files + bmp_files)
+# Get list of all image folders
+folder_list = ['baik', 'bangku', 'bel', 'dia', 'meja', 'pramuka', 'sakit', 'saya', 'teman', 'tugas']
 
-file_num = len(file_list)
-print('Total images: %d' % file_num)
+# Function to move files to destination folder
+def move_files(file_list, dest_folder):
+    for file_path in file_list:
+        filename = os.path.basename(file_path)
+        xml_file = os.path.splitext(filename)[0] + '.xml'
+        dest_file_path = os.path.join(dest_folder, filename)
+        dest_xml_path = os.path.join(dest_folder, xml_file)
+        os.rename(file_path, dest_file_path)
+        os.rename(os.path.join(file_path.parent, xml_file), dest_xml_path)
 
-# Determine number of files to move to each folder
-train_percent = 0.8  # 80% of the files go to train
-val_percent = 0.1 # 10% go to validation
-test_percent = 0.1 # 10% go to test
-train_num = int(file_num*train_percent)
-val_num = int(file_num*val_percent)
-test_num = file_num - train_num - val_num
-print('Images moving to train: %d' % train_num)
-print('Images moving to validation: %d' % val_num)
-print('Images moving to test: %d' % test_num)
+# Iterate through each folder
+for folder_name in folder_list:
+    # Create destination folders for current folder
+    train_folder_path = os.path.join(train_folder, folder_name)
+    val_folder_path = os.path.join(val_folder, folder_name)
+    test_folder_path = os.path.join(test_folder, folder_name)
+    os.makedirs(train_folder_path, exist_ok=True)
+    os.makedirs(val_folder_path, exist_ok=True)
+    os.makedirs(test_folder_path, exist_ok=True)
 
-# Select 80% of files randomly and move them to train folder
-for i in range(train_num):
-    move_me = random.choice(file_list)
-    fn = move_me.name
-    base_fn = move_me.stem
-    parent_path = move_me.parent
-    xml_fn = base_fn + '.xml'
-    os.rename(move_me, train_path+'/'+fn)
-    os.rename(os.path.join(parent_path,xml_fn),os.path.join(train_path,xml_fn))
-    file_list.remove(move_me)
+    # Get list of image files in current folder
+    image_files = list(Path(os.path.join(image_folder, folder_name)).rglob('*.jpg')) + \
+                  list(Path(os.path.join(image_folder, folder_name)).rglob('*.JPG')) + \
+                  list(Path(os.path.join(image_folder, folder_name)).rglob('*.png')) + \
+                  list(Path(os.path.join(image_folder, folder_name)).rglob('*.bmp'))
+    random.shuffle(image_files)
 
-# Select 10% of remaining files and move them to validation folder
-for i in range(val_num):
-    move_me = random.choice(file_list)
-    fn = move_me.name
-    base_fn = move_me.stem
-    parent_path = move_me.parent
-    xml_fn = base_fn + '.xml'
-    os.rename(move_me, val_path+'/'+fn)
-    os.rename(os.path.join(parent_path,xml_fn),os.path.join(val_path,xml_fn))
-    file_list.remove(move_me)
+    # Determine number of files to move to each folder
+    num_files = len(image_files)
+    train_num = int(num_files * 0.8)
+    val_num = int(num_files * 0.1)
+    test_num = num_files - train_num - val_num
 
-# Move remaining files to test folder
-for i in range(test_num):
-    move_me = random.choice(file_list)
-    fn = move_me.name
-    base_fn = move_me.stem
-    parent_path = move_me.parent
-    xml_fn = base_fn + '.xml'
-    os.rename(move_me, test_path+'/'+fn)
-    os.rename(os.path.join(parent_path,xml_fn),os.path.join(test_path,xml_fn))
-    file_list.remove(move_me)
+    # Move files to train folder
+    train_files = image_files[:train_num]
+    move_files(train_files, train_folder_path)
+
+    # Move files to validation folder
+    val_files = image_files[train_num:train_num+val_num]
+    move_files(val_files, val_folder_path)
+
+    # Move files to test folder
+    test_files = image_files[train_num+val_num:]
+    move_files(test_files, test_folder_path)
+
+    print('Images in folder %s moved to train: %d, validation: %d, test: %d' % (folder_name, train_num, val_num, test_num))
