31a7da10c084de0c3c428f1fcbcbba336fdfb6ca
nurullzzz <75351301+nurullzzz@users.noreply.github.com>
2023-06-08 02:04:15 +0800

    I gede bukan i kecil

--- convert_csv.py
+++ convert_csv.py
@@ -27,9 +27,9 @@ def xml_to_csv(path):
 
 def main():
     for folder in ['training','validation']:
-        image_path = os.path.join(os.getcwd(), ('images/' + folder))
+        image_path = os.path.join(os.getcwd(), ('Images/' + folder))
         xml_df = xml_to_csv(image_path)
-        xml_df.to_csv(('images/' + folder + '_labels.csv'), index=None)
+        xml_df.to_csv(('Images/' + folder + '_labels.csv'), index=None)
         print('Successfully converted xml to csv.')
 
 main()
